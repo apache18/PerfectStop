4e14105b0eccd30b311c555e9b470885e6db2d58
apache18 <savel.max2003@yandex.ru>
2022-06-18 13:28:03 +0400

    Анимация и смена дня
    Анимация машинки и смена дня и ночи

---
+++ PerfectStop/Assets/Scripts/timeChange.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class timeChange : MonoBehaviour
+{
+    public float rotatSpeed = 1f;
+    public Material material;
+
+    private void Start()
+    {
+        material.mainTextureOffset = new Vector2(Random.Range(0f, 0.2f), 0);
+    }
+
+    private void Update()
+    {
+        material.mainTextureOffset += new Vector2(rotatSpeed * Time.deltaTime, 0);
+    }
+}
