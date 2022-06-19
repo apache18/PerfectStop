0ebddd76f135ae7820d32bb4788854f47b480090
apache18 <savel.max2003@yandex.ru>
2022-06-19 13:51:54 +0400

    Функциональные кнопки и переход между сценами
    Добавили полную функциональность для кнопок на главной сцене, а  также переход между сценами

---
+++ PerfectStop/Assets/Scripts/CanvasButtons.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasButtons : MonoBehaviour
+{
+    public Sprite btn, btnPressed;
+
+    private Image _image;
+
+    private void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void PlayGame()
+    {
+        StartCoroutine(LoadScene("Game"));
+    }
+
+    public void SetPressedButton()
+    {
+        _image.sprite = btnPressed;
+        transform.GetChild(0).localPosition -= new Vector3(0, 7, 0);
+    }
+    public void SetDefaultButton()
+    {
+        _image.sprite = btn;
+        transform.GetChild(0).localPosition += new Vector3(0, 7, 0);
+    }
+
+    IEnumerator LoadScene(string name)
+    {
+        float fadeTime = Camera.main.GetComponent<Fading>().Fade(1f);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(name);
+    }
+}
